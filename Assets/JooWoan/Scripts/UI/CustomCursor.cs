b1f5203d57e1f8153b605f9fa26a7b823f28f509
catnip144 <skytower144@naver.com>
2025-01-22 10:15:51 +0900

    feat: 화면에 보이는 커서 추가

--- Assets/JooWoan/Scripts/UI/CustomCursor.cs
+++ Assets/JooWoan/Scripts/UI/CustomCursor.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using EverScord.Weapons;
 
 namespace EverScord.UI
 {
     public class CustomCursor : MonoBehaviour
     {
-        [SerializeField] private Transform aimPoint;
+        [SerializeField] private Weapon weapon;
         [SerializeField] private RectTransform canvasRect;
 
+        private Transform aimPoint;
         private RectTransform uiRect;
         private Camera mainCam;
 
@@ -14,6 +16,7 @@ namespace EverScord.UI
         {
             uiRect = GetComponent<RectTransform>();
             mainCam = Camera.main;
+            aimPoint = weapon.AimPoint;
         }
 
         void Update()
@@ -21,13 +24,15 @@ namespace EverScord.UI
             SetCursorPosition();
         }
 
+        // 0,0 for the canvas is at the center of the screen
         // WorldToViewPortPoint treats the lower left corner as 0,0
         private void SetCursorPosition()
         {
             Vector2 viewPortPosition = mainCam.WorldToViewportPoint(aimPoint.position);
+
             Vector2 screenPosition = new Vector2(
-                (viewPortPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f),
-                (viewPortPosition.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)
+                ((viewPortPosition.x - 0.5f) * canvasRect.sizeDelta.x),
+                ((viewPortPosition.y - 0.5f) * canvasRect.sizeDelta.y)
             );
 
             uiRect.anchoredPosition = screenPosition;
