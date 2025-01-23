5c1cb086e448a6d4b4fcbb775121fff5b8787ca7
catnip144 <skytower144@naver.com>
2025-01-23 17:52:15 +0900

    feat: 재장전 구현중

--- Assets/JooWoan/Scripts/Misc/ConstStrings.cs
+++ Assets/JooWoan/Scripts/Misc/ConstStrings.cs
@@ -6,7 +6,7 @@ namespace EverScord
         // Animation
         public const string ANIMATION_NED_SHOOT = "Ned_Shoot";
         public const string ANIMATION_NED_SHOOTEND = "Ned_ShootEnd";
-        public const string ANIMATION_NED_RELOAD = "Ned_Reload";
+        public const string ANIMATION_NED_RELOAD = "Ned_Shoot_Reload";
 
         // Animator Parameters
         public const string PARAM_ISMOVING = "isMoving";
