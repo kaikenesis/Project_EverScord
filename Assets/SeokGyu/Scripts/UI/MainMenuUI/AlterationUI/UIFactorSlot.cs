0dbb009218577fa0be23ce141c9663381816632a
kenesis <78710219+kaikenesis@users.noreply.github.com>
2025-02-11 16:06:55 +0900

    Reapply "Merge pull request #27 from kaikenesis/feature/SeokGyu"
    This reverts commit 0101336e42713e4ad8c2da783ff0daeeeacffdcf.

---
+++ Assets/SeokGyu/Scripts/UI/MainMenuUI/AlterationUI/UIFactorSlot.cs
@@ -0,0 +1,106 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EverScord
+{
+    public class UIFactorSlot : MonoBehaviour
+    {
+        public enum EType
+        {
+            ALPHA,
+            BETA,
+            MAX
+        }
+
+        [SerializeField] private Image backImg;
+        [SerializeField] private Image lockImg;
+        [SerializeField] private Image optionImg;
+        private EType curType;
+        private int slotNum;
+
+        public bool bLock { get; private set; }
+        public bool bConfirmed { get; private set; }
+
+        public static Action<int, int> OnClickedSlot = delegate { };
+        public static Action<int> OnDisplayOptionList = delegate { };
+
+        private void Awake()
+        {
+            UIFactor.OnRequestUnlock += HandleRequestUnlock;
+            UIFactor.OnApplyOption += HandleApplyOption;
+
+            bLock = true;
+            optionImg.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            UIFactor.OnRequestUnlock -= HandleRequestUnlock;
+            UIFactor.OnApplyOption -= HandleApplyOption;
+        }
+
+        #region Handle Methods
+        private void HandleRequestUnlock(int type, int slotNum)
+        {
+            if (bLock == true && curType == (EType)type && this.slotNum == slotNum)
+            {
+                bLock = false;
+                lockImg.enabled = false;
+            }
+        }
+
+        private void HandleApplyOption(int type, int slotNum, Color optionImgColor)
+        {
+            if (bLock == false && curType == (EType)type && this.slotNum == slotNum)
+            {
+                optionImg.enabled = true;
+                optionImg.color = optionImgColor;
+                Debug.Log("옵션 설정, 해당 옵션 이미지 Visible");
+            }
+        }
+        #endregion // Handle Methods
+
+        public void Initialize(EType type, bool bConfirmed, int slotIndex)
+        {
+            slotNum = slotIndex;
+            this.bConfirmed = bConfirmed;
+            bLock = !bConfirmed;
+            lockImg.enabled = !bConfirmed;
+            curType = type;
+
+            switch (curType)
+            {
+                case EType.ALPHA:
+                    backImg.color = new Color(1, 0.54f, 0.54f);
+                    break;
+                case EType.BETA:
+                    backImg.color = new Color(0.54f, 0.54f, 1);
+                    break;
+                default:
+                    backImg.color = new Color(1, 1, 1);
+                    break;
+            }
+        }
+
+        public void OnClicked()
+        {
+            OnClickedSlot?.Invoke((int)curType, slotNum);
+            if (bConfirmed)
+            {
+                OnDisplayOptionList?.Invoke((int)curType);
+            }
+            else
+            {
+                if (bLock == true)
+                {
+                    Debug.Log("Lock");
+                }
+                else
+                {
+                    Debug.Log("UnLock");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SeokGyu/Scripts/UI/MainMenuUI/RoomUI/UIDisplayRoom.cs
@@ -0,0 +1,209 @@
+using System;
+using UnityEngine;
+using Photon.Pun;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace EverScord
+{
+    public class UIDisplayRoom : MonoBehaviour
+    {
+        [SerializeField] private UIRoomPlayer uiRoomPlayerPrefab;
+        [SerializeField] private GameObject roomContainer;
+        [SerializeField] private GameObject inviteButton;
+        [SerializeField] private GameObject sendInvite;
+        [SerializeField] private GameObject[] hideObjects;
+        [SerializeField] private GameObject[] showObjects;
+        [SerializeField] private Button[] singleOnlyButtons;
+        [SerializeField] private Button[] masterOnlyButtons;
+        [SerializeField] private Button[] gameSettingButtons;
+        private UIRoomPlayer[] uiRoomPlayers;
+
+        public static Action OnLeaveRoom = delegate { };
+        public static Action OnVisibleObject = delegate { };
+
+        private void Awake()
+        {
+            PhotonRoomController.OnJoinRoom += HandleJoinRoom;
+            PhotonRoomController.OnRoomLeft += HandleRoomLeft;
+            PhotonRoomController.OnDisplayPlayers += HandleDisplayPlayers;
+            PhotonRoomController.OnUpdateRoom += HandleUpdateRoom;
+            PhotonMatchController.OnUpdateUI += HandleUpdateUI;
+
+            Init();
+        }
+
+        private void Start()
+        {
+            OnHideObjects();
+
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            PhotonRoomController.OnJoinRoom -= HandleJoinRoom;
+            PhotonRoomController.OnRoomLeft -= HandleRoomLeft;
+            PhotonRoomController.OnDisplayPlayers -= HandleDisplayPlayers;
+            PhotonRoomController.OnUpdateRoom -= HandleUpdateRoom;
+            PhotonMatchController.OnUpdateUI -= HandleUpdateUI;
+        }
+
+        private void Init()
+        {
+            uiRoomPlayers = new UIRoomPlayer[3];
+            for (int i = 0; i < 3; i++)
+            {
+                UIRoomPlayer uiRoomPlayer = Instantiate(uiRoomPlayerPrefab, roomContainer.transform);
+                uiRoomPlayers[i] = uiRoomPlayer;
+            }
+            inviteButton.transform.SetAsLastSibling();
+        }
+        #region Handle Methods
+        private void HandleJoinRoom()
+        {
+            gameObject.SetActive(true);
+
+            if (roomContainer != null)
+            {
+                roomContainer.SetActive(true);
+            }
+
+            OnShowObjects();
+        }
+
+        private void HandleRoomLeft()
+        {
+            gameObject.SetActive(false);
+
+            if (roomContainer != null)
+            {
+                roomContainer.SetActive(false);
+            }
+
+            OnHideObjects();
+        }
+
+        private void HandleDisplayPlayers(List<string> players)
+        {
+            int i = 0;
+            for (i = 0; i < players.Count; i++)
+            {
+                bool bMaster = false;
+                uiRoomPlayers[i].gameObject.SetActive(true);
+                if (PhotonNetwork.MasterClient.NickName == players[i])
+                    bMaster = true;
+                uiRoomPlayers[i].Initialize(players[i], bMaster);
+            }
+
+            for (; i < 3; i++)
+            {
+                uiRoomPlayers[i].gameObject.SetActive(false);
+            }
+
+            if (players.Count < 3)
+            {
+                inviteButton.SetActive(PhotonNetwork.IsMasterClient);
+            }
+            else
+            {
+                inviteButton.SetActive(false);
+            }
+        }
+
+        private void HandleUpdateRoom()
+        {
+            SetActiveMasterButton(PhotonNetwork.IsMasterClient);
+
+            if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
+            {
+                SetActiveSingleButton(false);
+            }
+            else
+            {
+                SetActiveSingleButton(true);
+            }
+        }
+
+        private void HandleUpdateUI(bool bActive)
+        {
+            SetActiveGameUI(bActive);
+        }
+        #endregion // Handle Methods
+
+        private void OnHideObjects()
+        {
+            for (int i = 0; i < hideObjects.Length; i++)
+            {
+                hideObjects[i].SetActive(false);
+            }
+        }
+
+        private void OnShowObjects()
+        {
+            for (int i = 0; i < showObjects.Length; i++)
+            {
+                showObjects[i].SetActive(true);
+            }
+
+            OnVisibleObject?.Invoke();
+        }
+
+        private void SetActiveMasterButton(bool bActive)
+        {
+            for (int i = 0; i < masterOnlyButtons.Length; i++)
+            {
+                masterOnlyButtons[i].interactable = bActive;
+            }
+        }
+
+        private void SetActiveSingleButton(bool bActive)
+        {
+            for (int i = 0; i < singleOnlyButtons.Length; i++)
+            {
+                singleOnlyButtons[i].interactable = bActive;
+            }
+        }
+
+        private void SetActiveGameUI(bool bActive)
+        {
+            // 파티장만 초대버튼 활성화/비활성화
+            // 나머지 버튼들은 비활성화
+            if(inviteButton.activeSelf == false)
+            {
+                inviteButton.SetActive(PhotonNetwork.IsMasterClient);
+            }
+            else if(inviteButton.activeSelf == true)
+            {
+                inviteButton.SetActive(false);
+                if (bActive == false)
+                {
+                    InitToggleButtons(inviteButton);
+                }
+            }
+
+            for (int i = 0; i < gameSettingButtons.Length; i++)
+            {
+                gameSettingButtons[i].interactable = bActive;
+                if(bActive == false)
+                {
+                    InitToggleButtons(gameSettingButtons[i].gameObject);
+                }
+            }
+        }
+
+        private void InitToggleButtons(GameObject gameObject)
+        {
+            UIToggleButton uiToggle = gameObject.gameObject.GetComponent<UIToggleButton>();
+            if (uiToggle != null)
+            {
+                //uiToggle.Init();
+            }
+        }
+
+        public void LeaveRoom()
+        {
+            OnLeaveRoom?.Invoke();
+        }
+    }
+}
