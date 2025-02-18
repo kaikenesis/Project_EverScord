ab25c88e973b8b038c6b695304bdf1903a3f77c6
Jang-wj <jwj5230@gmail.com>
2025-02-18 15:47:13 +0900

    feat : NMM2 구현 예정

---
+++ Assets/WooJeong/Scripts/NormalMonster/NMM2/NMM2_Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NMM2_Controller : NController
+{
+    protected override void Setup()
+    {
+        
+    }
+}
