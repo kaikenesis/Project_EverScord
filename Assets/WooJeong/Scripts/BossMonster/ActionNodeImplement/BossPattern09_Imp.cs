4ae7e78d66451ce0d2fd8459e8b3e59bdfa592ad
Jang-wj <jwj5230@gmail.com>
2025-03-05 09:28:27 +0900

    feat : 보스 패턴 9

--- Assets/WooJeong/Scripts/BossMonster/ActionNodeImplement/BossPattern09_Imp.cs
+++ Assets/WooJeong/Scripts/BossMonster/ActionNodeImplement/BossPattern09_Imp.cs
@@ -1,14 +1,26 @@
+using EverScord;
+using EverScord.Character;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossPattern09_Imp : ActionNodeImplement
 {
+    protected int attackCount = 30;
+
     protected override IEnumerator Act()
     {
+        List<CharacterControl> controls = new List<CharacterControl>();
+        foreach(var player in GameManager.Instance.PlayerDict.Values)
+        {
+            controls.Add(player);
+        }
+        int randInt = Random.Range(0, controls.Count);
+        controls[randInt].SetState(SetCharState.ADD, CharState.STUNNED);
+        bossRPC.PlayAnimation("Shoot");
+        yield return new WaitForSeconds(bossRPC.clipDict["Shoot"]);
         isEnd = true;
         action = null;
         yield return null;
     }
-
 }
