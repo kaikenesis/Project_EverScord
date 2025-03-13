8fdd0f71a6d89df83b34bee5f2620c714088959d
Jang-wj <jwj5230@gmail.com>
2025-03-13 15:01:03 +0900

    feat : 보스 스킬 데이터 작업 중

---
+++ Assets/WooJeong/Scripts/BossMonster/Data/BossSkillData.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+using EverScord;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Data/BossSkillData")]
+public class BossSkillData : ScriptableObject, IData
+{
+    public string Tag {  get; private set; }
+    public float Cooldown { get; private set; }
+    public float SkillRange { get; private set; }
+    public List<float> SkillSizes { get; private set; }
+    public float SkillDamage { get; private set; }
+    public float MaxHpBasedDamage { get; private set; }
+    public float SkillDotDamage { get; private set; }
+
+    public void Init()
+    {
+        MonsterSkillInfo skill = MonsterData.MonsterInfoDict[Tag];
+        Cooldown = skill.cooldown;
+        SkillRange = skill.skillRange;
+        SkillSizes = skill.skillSizes;
+        SkillDamage = skill.skillDamage;
+        MaxHpBasedDamage = skill.maxHpBasedDamage;
+        SkillDotDamage = skill.skillDotDamage;
+    }
+}
