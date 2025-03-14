ede821a234c852c47b16f387d7769ba1e4a5abcb
Jang-wj <jwj5230@gmail.com>
2025-03-14 16:18:35 +0900

    feat : DamageCalculator prototype

--- Assets/JooWoan/Scripts/Skill/DamageCalculator.cs
+++ Assets/JooWoan/Scripts/Skill/DamageCalculator.cs
@@ -1,5 +1,6 @@
 using EverScord.Character;
 using EverScord.Weapons;
+using UnityEngine;
 
 namespace EverScord.Skill
 {
@@ -21,6 +22,35 @@ namespace EverScord.Skill
 
             // Calculate damage
 
+            return totalDamage;
+        }
+
+        public static float GetAttackDamage(float attack, float critChance, float enemyDefense)
+        {
+            float defenseRate = enemyDefense / (enemyDefense + 100);
+            float damage = attack * (1 - defenseRate);
+
+            if(critChance > 0)
+            {
+                float randFloat = Random.Range(0.0f, 100.0f);
+                if (randFloat <= critChance)
+                    damage *= 1.2f;
+            }
+
+            int randInt = Random.Range(0, 100);
+            if (randInt < 5)
+                damage *= 3;
+            else if (randInt < 30)
+                damage *= 2;            
+
+            return damage;
+        }
+        
+        public static float GetSkillDamage(float attack, float skillDamage, float skillAttackRate, float skillDamageIncrease, float enemyDefense)
+        {
+            float defenseRate = enemyDefense / (enemyDefense + 100);
+            float damage = skillDamage + attack * skillAttackRate * defenseRate;
+            float totalDamage = damage + damage * (skillDamageIncrease * 0.01f);
             return totalDamage;
         }
     }
