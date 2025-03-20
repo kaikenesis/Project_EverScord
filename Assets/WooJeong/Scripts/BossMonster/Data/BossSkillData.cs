4de68d3280e5062642a9becef40351183b77fcea
Jang-wj <jwj5230@gmail.com>
2025-03-20 09:58:32 +0900

    feat : 난이도에 따른 수치 적용 (1.3배율)

--- Assets/WooJeong/Scripts/BossMonster/Data/BossSkillData.cs
+++ Assets/WooJeong/Scripts/BossMonster/Data/BossSkillData.cs
@@ -5,6 +5,7 @@ using EverScord;
 [CreateAssetMenu(menuName = "ScriptableObjects/Data/BossSkillData", order = 1)]
 public class BossSkillData : ScriptableObject, IData
 {
+    [SerializeField] float hardModeRate = 1.3f;
     [field : SerializeField] public string Tag {  get; private set; }
     [field: SerializeField] public float Cooldown { get; private set; }
     [field: SerializeField] public float SkillRange { get; private set; }
@@ -19,8 +20,19 @@ public class BossSkillData : ScriptableObject, IData
         Cooldown = skill.cooldown;
         SkillRange = skill.skillRange;
         SkillSizes = skill.skillSizes;
-        SkillDamage = skill.skillDamage;
-        MaxHpBasedDamage = skill.maxHpBasedDamage;
-        SkillDotDamage = skill.skillDotDamage;
+        
+
+        if (GameManager.Instance.PlayerData.difficulty == PlayerData.EDifficulty.Hard)
+        {
+            SkillDamage = skill.skillDamage * hardModeRate;
+            MaxHpBasedDamage = skill.maxHpBasedDamage * hardModeRate;
+            SkillDotDamage = skill.skillDotDamage * hardModeRate;
+        }
+        else
+        {
+            SkillDamage = skill.skillDamage;
+            MaxHpBasedDamage = skill.maxHpBasedDamage;
+            SkillDotDamage = skill.skillDotDamage;
+        }
     }
 }
