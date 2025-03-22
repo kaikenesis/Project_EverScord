5e9003966ced9158896f830bde4cdd959b288b3d
Jang-wj <jwj5230@gmail.com>
2025-03-22 15:46:06 +0900

    feat : DecreaseHP, IsDead == true 일 때 return

--- Assets/WooJeong/Scripts/NormalMonster/NController.cs
+++ Assets/WooJeong/Scripts/NormalMonster/NController.cs
@@ -235,9 +235,13 @@ public abstract class NController : MonoBehaviour, IEnemy
 
     public void DecreaseHP(float damage, CharacterControl attacker)
     {
+        if (isDead == true)
+            return;
+
         this.HP -= damage;
         if (this.HP <= 0)
         {
+            HP = 0;
             isDead = true;
             attacker.IncreaseKillCount();
             GameManager.Instance.LevelController.IncreaseMonsterProgress(monsterType);
@@ -252,9 +256,13 @@ public abstract class NController : MonoBehaviour, IEnemy
     [PunRPC]
     protected void SyncMonsterHP(float damage)
     {
+        if (isDead == true)
+            return;
+
         this.HP -= damage;
         if (this.HP <= 0)
         {
+            HP = 0;
             isDead = true;
             GameManager.Instance.LevelController.IncreaseMonsterProgress(monsterType);
         }
