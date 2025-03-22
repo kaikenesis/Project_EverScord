884d71803392e6c5fc08f18e34a22b1eac37846d
Jang-wj <jwj5230@gmail.com>
2025-03-22 15:01:43 +0900

    feat : 보스 패턴 14 도 점프 적용

--- Assets/WooJeong/Scripts/BossMonster/ActionNodeImplement/BossPattern14_Imp.cs
+++ Assets/WooJeong/Scripts/BossMonster/ActionNodeImplement/BossPattern14_Imp.cs
@@ -15,6 +15,20 @@ public class BossPattern14_Imp : AttackNodeImplement
     protected override IEnumerator Act()
     {
         Debug.Log("Attack14 start");
+
+        bossRPC.PlayAnimation("TakeOff");
+        bossRPC.PlayJumpEffect();
+        yield return new WaitForSeconds(bossRPC.clipDict["TakeOff"]);
+        transform.position = bossRPC.SpawnPos;
+        int randInt = Random.Range(0, 4);
+        transform.rotation = Quaternion.identity;
+        transform.Rotate(0, 90 * randInt, 0);
+        Debug.Log($"{randInt} : {90 * randInt}");
+        bossRPC.PlayAnimation("Landing");
+        yield return new WaitForSeconds(bossRPC.clipDict["Landing"] * 0.25f);
+        bossRPC.PlayJumpEffect();
+        yield return new WaitForSeconds(bossRPC.clipDict["Landing"] * 0.75f);
+
         bossRPC.PlayAnimation("RotatingShot", 0);
         bossRPC.PlaySound("BossLaserReady");
         yield return new WaitForSeconds(1.5f);
