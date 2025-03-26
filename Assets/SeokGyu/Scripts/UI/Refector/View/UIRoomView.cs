87f8af1b420a7e42409617bb873ea913441151b3
kenesis <78710219+kaikenesis@users.noreply.github.com>
2025-03-26 16:13:01 +0900

    feat : UI 개선 및 폰트 수정중

--- Assets/SeokGyu/Scripts/UI/Refector/View/UIRoomView.cs
+++ Assets/SeokGyu/Scripts/UI/Refector/View/UIRoomView.cs
@@ -3,12 +3,25 @@ using UnityEngine.UI;
 
 namespace EverScord
 {
-    public class UIRoomView : MonoBehaviour
+    public class UIRoomView : ToggleObject
     {
-        [field: SerializeField] public GameObject roomContainer;
-        [field: SerializeField] public Button[] singleOnlyButtons;
-        [field: SerializeField] public Button[] masterOnlyButtons;
-        [field: SerializeField] public Button[] gameSettingButtons;
+        [field: SerializeField] public GameObject RoomContainer;
+        [field: SerializeField] public GameObject InviteButton;
+        [field: SerializeField] public Button[] SingleOnlyButtons;
+        [field: SerializeField] public Button[] MasterOnlyButtons;
+        [field: SerializeField] public Button[] GameSettingButtons;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+
+        }
 
         public void DisplayRoom()
         {
